ab478eb7fa4225c2751e395ddc5f425bc48e86cc
Szymek <szymon.rzadca@gmail.com>
2018-06-20 10:16:37 +0200

    handling overflow exception, in fact redundant in python when using integers

--- examples.py
+++ examples.py
@@ -4,13 +4,12 @@ def factorial(n):
     if n <= 1:
         return 1
     else:
-        print(n / 0)
         return n * factorial(n-1)
 
 
 try:
-    print(factorial(1000))
-except (RecursionError, ZeroDivisionError):
+    print(factorial(900))
+except (RecursionError, OverflowError):
     print("This program cannot calculate factorials that large")
 
 print("Program terminated")
