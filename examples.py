afd2647704d8e6047b73d445d1640fcfa340fe7e
Szymek <szymon.rzadca@gmail.com>
2018-06-20 10:01:59 +0200

    exception handling example

---
+++ examples.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    # n! can also be defined as n * (n - 1)!
+    """calculates n! recursively"""
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+try:
+    print(factorial(900))
+except RecursionError:
+    print("This program cannot calculate factorials that large")
+
+print("Program terminated")
